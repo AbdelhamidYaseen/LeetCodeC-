df6535704dd1c9f336ab58dbf8f0ee5563cdc828
Peach <114141082+Vanroostyaseen@users.noreply.github.com>
2023-03-03 18:59:58 +0100

    Exc1

---
+++ LeetCodeCSharp/LeetCodeCSharp/Exercises.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    internal class Exercises
+    {
+        public static void Exc1() 
+        {
+            int[] nums1 = { 2, 7, 11, 15 };
+            int target1 = 9;
+            int[] nums2 = {3,2,4};
+            int target2 = 6;
+            int[] nums3 = {3,3};
+            int target3 = 6;
+            Console.WriteLine("" +
+                "Input: nums = [2,7,11,15], target = 9\r\n" + TwoSum(nums1, target1));
+            Console.WriteLine("NEXT");
+            Console.WriteLine("" +
+                "Input: nums = [3,2,4], target = 6\r\n " + TwoSum(nums2, target2));
+            Console.WriteLine("NEXT");
+            Console.WriteLine("" +
+                "Input: nums = [3,3], target = 6\r\n" + TwoSum(nums3,target3));
+        }
+        private static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target) 
+                    {
+                        Console.Write($"\n{i} || {j}\n");
+                        return new int[] { i, j };
+
+                    }
+                }
+            }
+            return Array.Empty<int>();
+
+
+        }
+    }
+}
---
+++ LeetCodeCSharp/LeetCodeCSharp/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace LeetCodeCSharp
+{
+    internal class Program
+    {
+        static void Wait() 
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("Press -ENTER- To Continue");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            string selected;
+            bool flick = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("======================================================");
+                Console.WriteLine("Please select a excercise");
+                Console.WriteLine("======================================================");
+                Console.WriteLine(
+                    "01| Exercise 1: \n" +
+                    "02| Exercise 2: \n" +
+                    "03| Exercise 3: \n" +
+                    "04| Exercise 4: \n" +
+                    "05| Exercise 5: \n" +
+                    "06| Exercise 6: \n" +
+                    "07| Exercise 7: \n" +
+                    "08| Exercise 8: \n" +
+                    "09| Exercise 9: \n" +
+                    "10| Exercise 10: \n" +
+                    "11| Exercise 11: \n" +
+
+                    "END| END-PROGRAM"
+                    );
+                Console.WriteLine("======================================================");
+                selected = Console.ReadLine();
+                Console.WriteLine("======================================================");
+                if (selected == "01")       { Console.Clear(); Exercises.Exc1(); Wait(); }
+                else if (selected == "02")  { Console.Clear(); }
+                else if (selected == "03")  { Console.Clear(); }
+                else if (selected == "04")  { Console.Clear(); }
+                else if (selected == "05")  { Console.Clear(); }
+                else if (selected == "06")  { Console.Clear(); }
+                else if (selected == "07")  { Console.Clear(); }
+                else if (selected == "08")  { Console.Clear(); }
+                else if (selected == "09")  { Console.Clear(); }
+                else if (selected == "10")  { Console.Clear(); }
+                else if (selected == "11")  { Console.Clear(); }
+                else if (selected == "12")  { Console.Clear(); }
+                else if (selected == "13")  { Console.Clear(); }
+                else if (selected == "14")  { Console.Clear(); }
+                else if (selected == "15")  { Console.Clear(); }
+                else if (selected == "16")  { Console.Clear(); }
+                else if (selected == "17")  { Console.Clear(); }
+                else if (selected == "18")  { Console.Clear(); }
+                else if (selected == "19")  { Console.Clear(); }
+                else if (selected == "20")  { Console.Clear(); }
+                else if (selected == "21")  { Console.Clear(); }
+                else if (selected == "22")  { Console.Clear(); }
+                else if (selected == "23")  { Console.Clear(); }
+                else if (selected == "24")  { Console.Clear(); }
+                else if (selected == "25")  { Console.Clear(); }
+
+                else if (selected == "END") { Console.Clear(); flick = false; }
+                else { Console.WriteLine("Please select a correct input"); }
+            }
+            while (flick);
+        }
+    }
+}
